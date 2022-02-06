8ff848227a9f8b43ab11604cd7eb1e8822d4d573
ivanovm <Imm8311@gmail.com>
2022-02-06 22:44:00 +0400

    Vars

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -2,6 +2,7 @@ package com.max.idea;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        double num = 13.1;
+        byte b = 127;
     }
 }
