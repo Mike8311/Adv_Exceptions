61a3844ec42a8fc3dce2fa5a98962ff06bfc3986
ivanovm <Imm8311@gmail.com>
2022-02-07 13:36:09 +0400

    Ops

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,13 +1,22 @@
 package com.max.idea;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int tmp = arr[0];
-        arr[0] = arr[arr.length - 1];
-        arr[arr.length - 1] = tmp;
-        System.out.println(arr[0] + arr[arr.length / 2]);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число x: ");
+        int x = scanner.nextInt();
+        System.out.print("Введите число y: ");
+        int y = scanner.nextInt();
+        System.out.print("Введите число z: ");
+        int z = scanner.nextInt();
+
+        int avg = (x + y + z) / 3;
+        System.out.println("Среднее арифметическое чисел x, y, z = " + avg);
+
+        if (avg / 2 > 3)
+            System.out.println("Программа выполнена корректно");
     }
 }
