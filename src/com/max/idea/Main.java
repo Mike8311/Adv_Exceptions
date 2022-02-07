0f77f81be1539bcd1ab42294100209aa898d798e
ivanovm <Imm8311@gmail.com>
2022-02-07 13:15:40 +0400

    Vars

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,8 +1,13 @@
 package com.max.idea;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        double num = 13.1;
-        byte b = 127;
+        int[] arr = {1, 2, 3, 4, 5};
+        int tmp = arr[0];
+        arr[0] = arr[arr.length - 1];
+        arr[arr.length - 1] = tmp;
+        System.out.println(arr[0] + arr[arr.length / 2]);
     }
 }
