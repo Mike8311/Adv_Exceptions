3f4b6b7c940ea04504be94cbee948a8c488aa45e
ivanovm <Imm8311@gmail.com>
2022-02-13 20:13:51 +0400

    Final2

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,19 +1,30 @@
 package com.max.idea;
 
-import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner( System.in );
 
-        System.out.print( "Курс доллара: ");
-        double k = scanner.nextDouble();
+        System.out.print( "Ввод: ");
+        String u = scanner.nextLine();
 
-        System.out.print( "Количество рублей: ");
-        double r = scanner.nextDouble();
+        int x = u.indexOf( 'x' );
+        int o = u.charAt( 1 ) == '+' ? 1 : -1;
+        int a = Character.getNumericValue( u.charAt( 0 ) );
+        int b = Character.getNumericValue( u.charAt( 2 ) );
+        int c = Character.getNumericValue( u.charAt( 4 ) );
 
-        System.out.printf("Итого: %.2f", r / k);
+        switch (x) {
+            case 0:
+                System.out.println( "Вывод: " + (c - b * o) );
+                break;
+            case 2:
+                System.out.println( "Вывод: " + (c - a) * o );
+                break;
+            case 4:
+                System.out.println( "Вывод: " + (a + b * o) );
+                break;
+        }
     }
 }
