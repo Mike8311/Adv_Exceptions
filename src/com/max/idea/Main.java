cb281630fd593308bc80ab85b123132045bdadad
ivanovm <Imm8311@gmail.com>
2022-02-13 15:55:24 +0400

    Data

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -6,21 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите количество строк матрицы: ");
-        int r = scanner.nextInt();
-        System.out.print("Введите количество столбцов матрицы: ");
-        int c = scanner.nextInt();
+        System.out.print("Введите первое число: ");
+        String s = scanner.next();
+        System.out.print("Введите второе число: ");
+        int i = scanner.nextInt();
 
-        int[][] mat = new int[r][c];
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                System.out.print("Введите элемент матрицы: ");
-                mat[i][j] = scanner.nextInt();
-            }
-        }
-
-        for (int i = 0; i < c; i++) {
-            System.out.println("Элемент * 3 равно: " + mat[0][i] * 3);
-        }
+        System.out.println("Большее число: " + Math.max(Integer.parseInt(s), i));
+        System.out.println("Меньшее число: " + (double)Math.min(Integer.parseInt(s), i));
     }
 }
