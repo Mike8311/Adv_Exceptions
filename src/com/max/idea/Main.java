a4e30a3bede4f1fba5cf1eb690e2b08789f649a0
ivanovm <Imm8311@gmail.com>
2022-02-13 12:31:53 +0400

    Cycles

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -6,46 +6,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        final int X = 0, Y = 1, Z = 2;
 
-        System.out.print("Выберите что переводить (0 - масса, 1 - расстояние): ");
-        int x = scanner.nextInt();
-        int y;
-        if (x == 0) {
-            System.out.print( "Выберите единицу измерения (0 - килограмм, 1 - карат, 2 - унция, 3 - фунт): " );
-            y = scanner.nextInt();
-            if (y < 0 || y > 3) {
-                System.out.println( "Вы ввели неверное число!" );
-                return;
+        System.out.print("Введите размер массива: ");
+        int l = scanner.nextInt();
+        int[] arr = new int[l];
+
+        for (int i = 0; i < l; i++) {
+            System.out.print("Введите элемент массива: ");
+            arr[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < l; i++) {
+            switch (arr[i]) {
+                case X:
+                case Y:
+                case Z:
+                    System.out.println("Данное значение имеется в константах");
+                    break;
             }
         }
-        else if (x == 1) {
-            System.out.print( "Выберите единицу измерения (0 - метр, 1 - миля, 2 - ярд, 3 - фут): " );
-            y = scanner.nextInt();
-            if (y < 0 || y > 3) {
-                System.out.println( "Вы ввели неверное число!" );
-                return;
-            }
-        }
-        else
-        {
-            System.out.println("Вы ввели неверное число!");
-            return;
-        }
-
-        System.out.print("Введите количество выбранных единиц: ");
-        int z = scanner.nextInt();
-
-        String[] weightNames = {"Килограмм", "Карат", "Унция", "Фунт"};
-        double[] weights = {1, 5000, 35.27396194958048, 2.2046226218488};
-
-        String[] lenNames = {"Метры", "Мили", "Ярды", "Футы"};
-        double[] lens = {1, 0.000621371192237334, 1.0936132983, 3.2808398950131};
-
-        String[] dimNames = x == 0 ? weightNames : lenNames;
-        double[] dims = x == 0 ? weights : lens;
-
-        System.out.println("Результат:");
-        for (int i = 0; i < dims.length; i++)
-            System.out.printf("%s: %.3f%n", dimNames[i], z / dims[y]  * dims[i]);
     }
 }
