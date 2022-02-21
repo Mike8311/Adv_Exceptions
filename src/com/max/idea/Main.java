2bf64c42a59cf9470549a2d87ecba14612fe6318
ivanovm <Imm8311@gmail.com>
2022-02-21 18:11:07 +0400

    Adv_Calc

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -5,34 +5,81 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner( System.in );
-        String question = "Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает";
-        String answer = "Заархивированный вирус";
 
-        System.out.println( "Отгдай загадку: " + question);
+        System.out.print( "Введите дробное число: ");
+        double d1 = scanner.nextDouble();
 
-        for (int i = 1; i <= 3; i++) {
-            System.out.println( "Введи ответ: " );
-            String s = scanner.nextLine();
-            if (s.equals( "Подсказка" )) {
-                if (i == 1) {
-                    System.out.println( "Подсказка: Заархивированный ..." );
-                } else
-                    System.out.println( "Подсказка уже недоступна" );
-                System.out.println( "Введи ответ: " );
-                s = scanner.nextLine();
-                if (!s.equals( answer ) && i == 1)
-                    break;
-            }
+        System.out.print( "И еще одно: ");
+        double d2 = scanner.nextDouble();
 
-            if (s.equals( answer ))
-            {
-                System.out.println( "Правильно!" );
-                return;
-            }
-            else if (i < 3)
-                System.out.println( "Подумай еще!" );
+        System.out.print( "Введите операцию (+,-,/,*): ");
+        String op = scanner.next();
+
+        switch (op) {
+            // Сложение
+            case "+":
+                System.out.printf( "Сумма двух чисел: %.4f", sum(d1, d2) );
+                break;
+
+            // Вычитание
+            case "-":
+                System.out.printf( "Разница двух чисел: %.4f", sub(d1, d2) );
+                break;
+
+            // Деление
+            case "/":
+                System.out.printf( "Результат деления двух чисел: %.4f", div(d1, d2) );
+                break;
+
+            // Умножение
+            case "*":
+                System.out.printf( "Произведение двух чисел: %.4f", mul(d1, d2) );
+                break;
         }
-
-        System.out.println( "Обидно, приходи в другой раз" );
     }
+
+    /**
+     * Сложение двух дробных чисел
+     * @param d1 первое число
+     * @param d2 второне число
+     * @return сумма двух чисел
+     */
+    public static double sum(double d1, double d2)
+    {
+        return d1 + d2;
+    }
+
+    /**
+     * Вычитание двух дробных чисел
+     * @param d1 первое число
+     * @param d2 второне число
+     * @return разница двух чисел
+     */
+    public static double sub(double d1, double d2)
+    {
+        return d1 - d2;
+    }
+
+    /**
+     * Деление двух дробных чисел
+     * @param d1 первое число
+     * @param d2 второне число
+     * @return результат деления двух чисел
+     */
+    public static double div(double d1, double d2)
+    {
+        return d1 / d2;
+    }
+
+    /**
+     * Умножение двух дробных чисел
+     * @param d1 первое число
+     * @param d2 второне число
+     * @return произведение двух чисел
+     */
+    public static double mul(double d1, double d2)
+    {
+        return d1 * d2;
+    }
+
 }
