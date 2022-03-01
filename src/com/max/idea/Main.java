1555210cbe82048165dd071e053f76d62f99c4e7
ivanovm <Imm8311@gmail.com>
2022-03-01 18:08:32 +0400

    Adv_Exceptions

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -6,11 +6,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner( System.in );
-
-        calculator( scanner );
-
-        scanner.close();
+        try (Scanner scanner = new Scanner( System.in )) {
+            calculator( scanner );
+        }
     }
 
     /**
